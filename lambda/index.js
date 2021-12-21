99d99789031f7fde2b5430221928fa85d1460606
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-12-21 14:30:26 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -10,7 +10,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Well howdy, you can say Hello or Help. Which would you like to try?';
+        const speakOutput = 'Well howdy, let me help you find your pace. Which would you like to try?';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
