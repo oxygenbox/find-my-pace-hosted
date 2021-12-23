8d90e1e064078540a0039a7535c11070b97f94a5
Michael Schaffner <michaelschaffner@Michaels-MacBook-Pro.local>
2021-12-23 08:12:03 -0600

    removed HelloWorldIntent

--- lambda/index.js
+++ lambda/index.js
@@ -100,21 +100,6 @@ const SetDistanceIntentHandler = {
         }
 };
 
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
-
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -225,7 +210,6 @@ const ErrorHandler = {
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        HelloWorldIntentHandler,
         SetTimeIntentHandler,
         SetDistanceIntentHandler,
         HelpIntentHandler,
