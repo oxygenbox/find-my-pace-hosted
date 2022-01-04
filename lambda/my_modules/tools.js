67b561f5c6f30a99aaba53903daa8a2530ca655c
Michael Schaffner <michaelschaffner@Michaels-MacBook-Pro.local>
2022-01-04 14:14:31 -0600

    fix errors

--- lambda/my_modules/tools.js
+++ lambda/my_modules/tools.js
@@ -102,10 +102,10 @@ var tools = {
     },
 
     singularUnit(atttribute){
-       return.attributes.units.slice(0, -1)
+       return attributes.units.slice(0, -1)
     },
 
-    function secondsToTimeString(seconds) {
+    secondsToTimeString(seconds) {
         return new Date(seconds * 1000).toISOString().substr(11, 8);
     },
     
